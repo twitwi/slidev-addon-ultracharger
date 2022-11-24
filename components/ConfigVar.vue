1d6e8650636073fb24ce5ebc8e2a1efc266f2278
Rémi Emonet <remi.emonet@reverse--com.heeere>
2022-11-24 14:42:59 +0100

    Add ConfigVar component to access headmatter easily

---
+++ components/ConfigVar.vue
@@ -0,0 +1,20 @@
+<template>
+    <component :is="type" v-html="content" />
+</template>
+<script lang="ts" setup>
+import { computed } from '@vue/reactivity';
+
+
+const props = defineProps({
+    name: { default: undefined },
+    type: { default: "span"},
+    nbsp: { default: " "},
+})
+
+const content = computed(() => {
+    console.log(props.name)
+    let v = $slidev.configs[props.name]
+    v = v.replaceAll(/ /g, props.nbsp)
+    return v
+})
+</script>
