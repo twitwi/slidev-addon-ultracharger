d29fde4480bfd1e8a33cfc848bef590e900fd584
Rémi Emonet <remi.emonet@reverse--com.heeere>
2022-12-22 00:04:57 +0100

    Make global-top tunable (via addonsConfigs.ultracharger)

--- global-top.vue
+++ global-top.vue
@@ -1,18 +1,24 @@
 <template>
   <footer
-    v-if="$slidev.nav.currentLayout !== 'cover'"
+    v-if="has('metaFooter') && ($slidev.nav.currentLayout !== 'cover')"
     class="absolute bottom-0 text-center left-0 right-0 px-2 py-0 text-xs"
       >
       <button @click="$slidev.nav.openInEditor()" title="Open in Editor" class="icon-btn opacity-20 !border-none !hover:text-white p-0">
           <carbon:edit />
       </button>
-      {{ "date author".split(" ").map(k => $slidev.configs[k]).join(" − ") }}
+      {{ (conf('metaFooter') ?? "date author morefooter").split(" ").map(k => $slidev.configs[k]).filter(v => !!v).join(" − ") }}
       − 
       {{ $slidev.nav.currentPage }} / {{ $slidev.nav.total+1 }}
   </footer>
 
-  <AutoPlay />
-  <!--TocNG mode="allDots" :max-depth="1" class="absolute bottom-0 right-5"/-->
-  <TOC mode="dots" class="absolute bottom-0 right-5"/>
-  <Blackout/>
+  <!--AutoPlay /-->
+  <TocNG
+    v-if="has('tocFooter')"
+    mode="allDots" :max-depth="1" class="absolute bottom-0 right-5"/>
+  <!--TOC mode="dots" class="absolute bottom-0 right-5" /-->
+  <Blackout v-if="has('blackout')"/>
 </template>
+<script setup lang="ts">
+const conf = (k) => $slidev.configs.addonsConfig?.ultracharger?.[k]
+const has = (feat) => ! conf('disable')?.includes(feat)
+</script>
