0aab9d8e2d4fe044ac5a3a41bd59f1d08779c395
Rémi Emonet <remi.emonet@reverse--com.heeere>
2023-11-24 17:18:56 +0100

    Adding a Alt component

---
+++ components/Alt.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+let altParam = $slidev?.configs?.addonsConfig?.ultracharger?.altParam ?? 'alt'
+let forceAlt = $slidev?.configs?.addonsConfig?.ultracharger?.alt ?? false
+
+const el = ref()
+
+onMounted(() => {
+    const params = new URLSearchParams(globalThis.document.location.search)
+    if (forceAlt || params.has(altParam)) {
+        const span = el.value
+        span.parentNode.innerHTML = span.innerHTML
+    }
+})
+</script>
+<template>
+    <span ref="el" class="unwrapped-or-display-none" style="display: none">
+        <slot></slot>
+    </span>
+</template>
