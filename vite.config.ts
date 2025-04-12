0ebf47ea2c2b8f4dc06fa1a665bb205d136959a0
Rémi Emonet <remi.emonet@reverse--com.heeere>
2025-04-12 10:22:36 +0200

    Reformat vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from "vite";
 
 export default defineConfig({
   slidev: {
@@ -8,25 +8,31 @@ export default defineConfig({
     markdown: {
       /* markdown-it options */
       markdownItSetup(md) {
-        md.renderer.rules.step_marker = () => '<span class="step"></span>'
-        md.inline.ruler.after('text', 'step_symbol', function replace(state) {
-            let ch, code, match, pos = state.pos, max = state.posMax;
+        md.renderer.rules.step_marker = () => '<span class="step"></span>';
+        md.inline.ruler.after("text", "step_symbol", function replace(state) {
+          let ch,
+            code,
+            match,
+            pos = state.pos,
+            max = state.posMax;
 
-            if (state.src.charCodeAt(pos) !== 0x26/* & */) { return false; }
-            if (pos + 1 < max) {
-                ch = state.src.charCodeAt(pos + 1);
-                if (ch === 124 /* | */) {
-                    state.push('step_marker')
-                    //if (!silent) {
-                    //}
-                    state.pos += 2
-                    return true
-                  }
-                }
-            return false
-        })
-      }
+          if (state.src.charCodeAt(pos) !== 0x26 /* & */) {
+            return false;
+          }
+          if (pos + 1 < max) {
+            ch = state.src.charCodeAt(pos + 1);
+            if (ch === 124 /* | */) {
+              state.push("step_marker");
+              //if (!silent) {
+              //}
+              state.pos += 2;
+              return true;
+            }
+          }
+          return false;
+        });
+      },
     },
     /* options for other plugins */
   },
-} as UserConfig)
+} as UserConfig);
